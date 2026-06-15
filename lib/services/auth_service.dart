9956b19af60eac9f2b179715374286675e51e900
James Evans <james22040@gmail.com>
2026-06-15 02:03:21 +0100

    Apple Sign In working on iOS

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 
 class AuthService {
 
@@ -87,8 +91,43 @@ class AuthService {
 // APPLE LOGIN
 // =====================================================
 
+String _generateNonce([int length = 32]) {
+
+  const charset =
+      '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._';
+
+  final random = Random.secure();
+
+  return List.generate(
+    length,
+    (_) => charset[
+        random.nextInt(charset.length)],
+  ).join();
+}
+
+String _sha256ofString(
+  String input,
+) {
+
+  final bytes =
+      utf8.encode(input);
+
+  final digest =
+      sha256.convert(bytes);
+
+  return digest.toString();
+}
+
 Future<UserCredential>
     signInWithApple() async {
+
+  final rawNonce =
+      _generateNonce();
+
+  final nonce =
+      _sha256ofString(
+    rawNonce,
+  );
 
   final appleCredential =
       await SignInWithApple
@@ -97,15 +136,19 @@ Future<UserCredential>
       AppleIDAuthorizationScopes.email,
       AppleIDAuthorizationScopes.fullName,
     ],
+    nonce: nonce,
   );
 
-  final oauthCredential =
-      OAuthProvider("apple.com")
-          .credential(
-    idToken:
-        appleCredential.identityToken,
-    rawNonce: null,
-  );
+  if (appleCredential.identityToken == null) {
+  throw Exception('Apple identity token was null');
+}
+
+final oauthCredential =
+    OAuthProvider('apple.com').credential(
+  idToken: appleCredential.identityToken!,
+  accessToken: appleCredential.authorizationCode,
+  rawNonce: rawNonce,
+);
 
   return await _auth
       .signInWithCredential(
